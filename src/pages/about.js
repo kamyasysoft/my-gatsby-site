9164daadb52a3b99cf7453de178d564a6a82574b
kamyasysoft <kamyasysoft@gmail.com>
2021-12-01 10:15:51 +0530

    update .mdx files and updates in some files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,36 +1,36 @@
 // // Step 1: Import React
 // import * as React from 'react'
-// import {Link} from 'gatsby';
+// import { Link } from 'gatsby'
 
 // // Step 2: Define your component
-// const IndexPage = () => {
+// const AboutPage = () => {
 //   return (
 //     <main>
-//       <title>Home Page</title>
-//       <h1>Welcome to my Gatsby site!</h1>
-//       <Link to="/about">About</Link>
-//       <p>I'm making this by following the Gatsby Tutorial.</p>
+//       <title>About Us</title>
+//       <h1>About Us</h1>
+//       <Link to="/">Back to Home Page</Link>
+//       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
 //     </main>
 //   )
 // }
 
 // // Step 3: Export your component
-// export default IndexPage
+// export default AboutPage
 
 import * as React from 'react';
 import Layout from '../components/Layout';
 import { StaticImage } from 'gatsby-plugin-image';
 
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-  <Layout pageTitle="Home Page">
+  <Layout pageTitle="About Us Page">
     <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     <StaticImage
       alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-      src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      src="../images/th.jpg"
     />
   </Layout>
   )
 }
 
-export default IndexPage
+export default AboutPage
